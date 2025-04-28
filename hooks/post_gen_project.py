8ff0a191cb4022d97756cd4787535c56c57dff43
moojor224 <moojor224@gmail.com>
2025-04-28 15:45:30 -0500

    Re-enable automatic npm install and remove git remote add

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,15 +1,22 @@
 import subprocess
 
-subprocess.call(["git", "init"])
-subprocess.call(
-    [
-        "git",
-        "remote",
-        "add",
-        "origin",
-        "https://github.com/{{cookiecutter.author}}/{{cookiecutter.extension_id}}.git",
-    ]
-)
-subprocess.call(["git", "add", "."])
-subprocess.call(["git", "commit", "-m", "Initial commit"])
-# subprocess.call(["npm", "install"])
+try:
+    subprocess.check_call(["git", "init"])
+    # subprocess.call(
+    #     [
+    #         "git",
+    #         "remote",
+    #         "add",
+    #         "origin",
+    #         "https://github.com/{{cookiecutter.author}}/{{cookiecutter.extension_id}}.git",
+    #     ]
+    # )
+    subprocess.check_call(["git", "add", "."])
+    subprocess.check_call(["git", "commit", "-m", "Initial commit"])
+except subprocess.CalledProcessError:
+    pass # ignore error if git not installed
+
+try:
+    subprocess.check_call(["npm", "install"])
+except subprocess.CalledProcessError:
+    pass # ignore error if npm not installed
